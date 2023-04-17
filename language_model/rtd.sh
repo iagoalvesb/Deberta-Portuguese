a56ac3b65e70c2a8898b8691d3b676b81780ba1c
iagoalvesb <71942285+iagoalvesb@users.noreply.github.com>
2023-04-16 21:10:32 -0300

    Add files via upload

--- language_model/rtd.sh
+++ language_model/rtd.sh
@@ -6,6 +6,14 @@ cd $SCRIPT_DIR
 cache_dir=/tmp/DeBERTa/RTD/
 
 max_seq_length=512
+
+num_training_steps=100
+num_train_epochs=1
+learning_rate=1e-4
+train_batch_size=6
+
+
+
 data_dir=$cache_dir/dataset
 
 function setup_wiki_data(){
@@ -26,75 +34,21 @@ Task=RTD
 init=$1
 tag=$init
 case ${init,,} in
-	deberta-v3-xsmall-continue)
-	# wget https://huggingface.co/microsoft/deberta-v3-xsmall/resolve/main/pytorch_model.generator.bin
-	# wget https://huggingface.co/microsoft/deberta-v3-xsmall/resolve/main/pytorch_model.bin
-	parameters=" --num_train_epochs 1 \
-	--model_config rtd_xsmall.json \
-	--warmup 10000 \
-	--num_training_steps 100000 \
-	--learning_rate 5e-5 \
-	--train_batch_size 256 \
-	--init_generator <TODO: generator checkpoint> \
-	--init_discriminator <TODO: discriminator checkpoint> \
-	--decoupled_training True \
-	--fp16 True "
-		;;
-	deberta-v3-xsmall)
-	parameters=" --num_train_epochs 1 \
-	--model_config rtd_xsmall.json \
-	--warmup 10000 \
-	--learning_rate 3e-4 \
-	--train_batch_size 64 \
-	--decoupled_training True \
-	--fp16 True "
-		;;
-	deberta-v3-small-continue)
-	# wget https://huggingface.co/microsoft/deberta-v3-small/resolve/main/pytorch_model.generator.bin
-	# wget https://huggingface.co/microsoft/deberta-v3-small/resolve/main/pytorch_model.bin
-	parameters=" --num_train_epochs 1 \
-	--model_config rtd_small.json \
-	--warmup 10000 \
-	--num_training_steps 100000 \
-	--learning_rate 5e-5 \
-	--train_batch_size 256 \
-	--init_generator <TODO: generator checkpoint> \
-	--init_discriminator <TODO: discriminator checkpoint> \
-	--decoupled_training True \
-	--fp16 True "
-		;;
 	deberta-v3-base)
-	parameters=" --num_train_epochs 5 \
+	parameters=" --num_train_epochs $num_train_epochs \
 	--model_config rtd_base.json \
 	--warmup 0.01 \
-	--learning_rate 1e-4 \
-	--train_batch_size 8 \
+	--learning_rate $learning_rate \
+	--train_batch_size $train_batch_size \
 	--decoupled_training True \
 	--fp16 True "
-		;;
-	deberta-v3-large)
-	parameters=" --num_train_epochs 1 \
-	--model_config rtd_large.json \
-	--warmup 10000 \
-	--learning_rate 1e-4 \
-	--train_batch_size 256 \
-	--decoupled_training True \
-	--fp16 True "
-		;;
-	*)
-		echo "usage $0 <Pretrained model configuration>"
-		echo "Supported configurations"
-		echo "deberta-v3-xsmall - Pretrained DeBERTa v3 XSmall model with 9M backbone network parameters (12 layers, 256 hidden size) plus 32M embedding parameters(128k vocabulary size)"
-		echo "deberta-v3-xsmall - Pretrained DeBERTa v3 Base model with 81M backbone network parameters (12 layers, 768 hidden size) plus 96M embedding parameters(128k vocabulary size)"
-		echo "deberta-v3-xsmall - Pretrained DeBERTa v3 Large model with 288M backbone network parameters (24 layers, 1024 hidden size) plus 128M embedding parameters(128k vocabulary size)"
-		exit 0
 		;;
 esac
 
 python -m DeBERTa.apps.run --model_config config.json  \
 	--tag $tag \
 	--do_train \
-	--num_training_steps 10  \
+	--num_training_steps $num_training_steps  \
 	--max_seq_len $max_seq_length \
 	--dump 10000 \
 	--task_name $Task \
