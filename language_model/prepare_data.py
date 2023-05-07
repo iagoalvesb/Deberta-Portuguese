78b9c610ff0942015f8e02c6d314eb4510e7121c
Gabriel da Mata Marques <70957747+Perkhad@users.noreply.github.com>
2023-05-07 20:20:30 -0300

    new load method and tokenizer class

--- language_model/prepare_data.py
+++ language_model/prepare_data.py
@@ -4,34 +4,39 @@ from DeBERTa import deberta
 import sys
 import argparse
 from tqdm import tqdm
+import sentencepiece as spm
 
 import sys
 
+class Tokenizer(object):
+    def __init__(self, tokenizer_path) -> None:
+        self.tokenizer_path = tokenizer_path
+        self.sp = spm.SentencePieceProcessor(model_file=self.tokenizer_path)
+
+    def tokenize(self, l) -> list:
+        tokens = self.sp.encode_as_ids(l)
+        return tokens
+
 def tokenize_data(input, output=None, max_seq_length=512):  
 #   vocab_path, vocab_type = deberta.load_vocab(vocab_path='tokenizer/spm.model', vocab_type='spm', pretrained_id='deberta-v3-base')
-  vocab_path, vocab_type = deberta.load_vocab(vocab_path='tokenizer/spm.model', vocab_type='spm')
-  tokenizer = deberta.tokenizers[vocab_type](vocab_path)
-  if output is None:
-    output=input + '.spm'
-  all_tokens = []
-  with open(input, encoding = 'utf-8') as fs:
-    for l in tqdm(fs, ncols=80, desc='Loading'):
-      if len(l) > 0:
-        tokens = tokenizer.tokenize(l)
-      else:
-        tokens = []
-      all_tokens.extend(tokens)
+  remaining_tokens = []
+  tokenizer = Tokenizer('deBeRta')
 
-  print(f'Loaded {len(all_tokens)} tokens from {input}')
-  lines = 0
-  with open(output, 'w', encoding = 'utf-8') as wfs:
-    idx = 0
-    while idx < len(all_tokens):
-      wfs.write(' '.join(all_tokens[idx:idx+max_seq_length-2]) + '\n')
-      idx += (max_seq_length - 2)
-      lines += 1
+  with open('data_debertav3/train_wiki_brwac.raw', encoding = 'utf-8') as fs:
+      with open('deberta_v3_pt_tokenized/train.txt', 'w', encoding = 'utf-8') as wfs:
+          for l in tqdm(fs, ncols=80, desc='Loading'):
+              if len(l) > 0:
+                  tokens = tokenizer.tokenize(l)
+              else:
+                  tokens = []
 
-  print(f'Saved {lines} lines to {output}')
+              remaining_tokens.extend(tokens)
+
+              while len(remaining_tokens) >= 510:
+                  wfs.write(' '.join(remaining_tokens[:510]) + '\n')
+                  remaining_tokens = remaining_tokens[510:]
+
+  #print(f'Saved {lines} lines to {output}')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', required=True, help='The input data path')
